82f9764890fa1a0c8d7a24c06761c4806e29da2b
Baltazar-Figuereo <Bfiguereo40@gmail.com>
2022-03-04 13:45:32 -0500

    Added root and message routes

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const app = express();
 
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+    res.status(200).send("Hello world")
+});
+
+app.get('/messages', (req, res) => {
+    res.status(200).send("<ul><li>Hello Albert!!!</li></ul>")
+})
+
+app.post("messages", (req, res) => {
+    console.log('updating message')
+})
+
 app.listen(PORT, () => {
     console.log(`Running the server at port ${PORT}`)
 })
