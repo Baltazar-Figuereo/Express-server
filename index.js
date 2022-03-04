b3580eab004454fdb6cd59b38cd5ccfd40cd0b4e
Baltazar-Figuereo <Bfiguereo40@gmail.com>
2022-03-04 14:11:36 -0500

    Added friend routes

--- index.js
+++ index.js
@@ -4,12 +4,36 @@ const app = express();
 
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-    res.status(200).send({
-        id: 1,
-        name: "Sir Isaac Newton"
-    })
+const friends = [
+    {
+      id: 0,
+      name: 'Albert Einstein'
+    },
+    {
+      id: 1,
+      name: 'Sir Isaac Newton'
+    }
+];
+
+app.get("/friends", (req, res) => {
+    res.status(200).json(friends);
 });
+
+app.get("/friends/:friendId", (req, res) => {
+    const friendId = Number(req.params.friendId);
+    const friend = friends[friendId];
+
+    if (friend) {
+        res.json(friend);
+    }
+    else {
+        res.status(404).json({
+            error: "Friend does not exist"
+        });
+    }
+
+    // console.log(friendId);
+})
 
 app.get('/messages', (req, res) => {
     res.send("<ul><li>Hello Albert!!!</li></ul>")
